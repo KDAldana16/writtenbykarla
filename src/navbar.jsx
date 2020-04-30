7593dbfbf9eaf47a388269e036a9138f014bb5d7
Karla Aldana <karla.aldana@students.dominican.edu>
2020-04-30 13:06:27 -0700

    attempt at navbar

---
+++ src/navbar.jsx
@@ -0,0 +1,17 @@
+// src/navbar.jsx
+
+import React from 'react'
+import './navbar.css'
+
+class Navbar extends Component {
+  render() {
+    return (
+      <React.Fragment>
+      <nav className="site-header sticky-top mb-5">
+
+      </nav>
+    );
+  }
+}
+
+export default Navbar;
